fa585a35a6ad38e18d4b1dc99f5f402722044472
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-10-25 17:31:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-import math
-class Point:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-    def printpoint(self):
-        return self.x, self.y
-    def distance(a,b):
-        return math.sqrt((b.x-a.x)**2+(b.y-a.y)**2)
+def nwd(a,b):
+    while b:
+        a, b = b, a %b
+        return a
 
-a=Point(5,0)
-print(Point.printpoint(a))
-b=Point(2,0)
-print(Point.distance(a,b))
+class wymierna:
+    licznik:int
+    mianownik:int
+    def __init__(self,licznik=0,mianownik=1):
+        self.licznik=licznik
+        if mianownik==0:
+            print("tak nie wolno")
+        else:
+            self.mianownik=mianownik
+    def get_licznik(self):
+        return self.licznik
+    def get_mianownik(self):
+        return self.mianownik
+    def __repr__(self):
+        return f'{self.licznik}/{self.mianownik}'
+    def __float__(self):
+        return self.licznik/self.mianownik
+
+    def __add__(self, other):
+        if self.mianownik!=other.mianownik:
+            lewy=self.licznik*other.mianownik
+            prawy=other.licznik*self.mianownik
+            return wymierna(lewy+prawy,self.mianownik*other.mianownik)
+        else:
+            return wymierna(self.licznik + other.licznik,self.mianownik)
+    def __sub__(self,other):
+        if self.mianownik!=other.mianownik:
+            lewy=self.licznik*other.mianownik
+            prawy=other.licznik*self.mianownik
+            return wymierna(lewy-prawy,self.mianownik*other.mianownik)
+        else:
+            return wymierna(self.licznik - other.licznik,self.mianownik)
 
 
 
 
 
 
+
+
+
+def main():
+    liczba1=wymierna(licznik=4,mianownik=6)
+    print(liczba1.get_licznik())
+    print(liczba1.get_mianownik())
+    print(liczba1.__repr__())
+    liczba2=wymierna(licznik=2,mianownik=3)
+    print(liczba1.__add__(liczba2))
+    print(liczba2.__sub__(liczba1))
+
+
+
+
+if __name__=="__main__":
+    main()
